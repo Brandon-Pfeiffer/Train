8c15d2a671d10c87de7d201174b4fe58f2439ee0
Brandon Pfeiffer <brandonpfeiffer@Brandons-MBP.attlocal.net>
2017-10-20 19:34:53 -0400

    bootstrap finished

---
+++ logic.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyAVGmdHI_OOWwfU9pwh_wSdkq6AHUpQmM8",
+    authDomain: "train-60af8.firebaseapp.com",
+    databaseURL: "https://train-60af8.firebaseio.com",
+    projectId: "train-60af8",
+    storageBucket: "train-60af8.appspot.com",
+    messagingSenderId: "1065719042163"
+  };
+  firebase.initializeApp(config);
+
+  var 
